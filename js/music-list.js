311bc492a337936e7789c99c9991873a26533a2f
Sourabh-513 <54619080+Sourabh-513@users.noreply.github.com>
2022-06-05 15:08:10 +0530

    Update music-list.js

--- js/music-list.js
+++ js/music-list.js
@@ -17,6 +17,13 @@ let allMusic = [
     src: "music-1"
   },
   
+  {
+    name: "कोकणातला रहस्यमय वाडा (Disha Joshi)",
+    artist: "कोकणातल्या अशाच रहस्यमय कथा ऐकण्यासाठी आत्ताच Google Play Store वरती Dark Kokan 2.0 Search करुन ॲप फ्री डाऊनलोड करा",
+    img: "music-2",
+    src: "music-2"
+  },
+  
   // like this paste it and remember to give comma after ending of this bracket }
   // {
   //   name: "Here is the music name",
